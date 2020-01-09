217dd738effa08520be9b565f71af4c94c44f210
alannguyen00 <58610714+alannguyen00@users.noreply.github.com>
2020-01-09 14:00:11 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+})
+function projectile2 () {
+    projectile = sprites.createProjectileFromSide(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . 7 7 7 7 . . . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . 7 f 1 7 7 7 . . . . . 
+. . . . . 7 1 1 7 7 7 . . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . . 7 7 7 7 . . . . . . 
+. . . . . . . 7 7 7 7 . d . . . 
+. . . . . . . . . 7 d d d . . . 
+. . . . . . . . 7 d d d 1 . . . 
+. . . . . . . . . d d d 1 . . . 
+. . . . . . . . . . 1 1 1 d . . 
+. . . . . . . . . . . d . . . . 
+`, 50, 0)
+    projectile.setPosition(0, Math.randomRange(-50, 120))
+}
 function player () {
     Victim = sprites.create(img`
 . . . . . . . . . . . . . 9 9 . 
@@ -20,32 +44,8 @@ function player () {
     Victim.setPosition(74, 55)
     controller.moveSprite(Victim, 100, 100)
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
-    info.changeScoreBy(1)
-})
-function projectile () {
-    projectile2 = sprites.createProjectileFromSide(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . f . . . . . . . 
-. . . . . . 7 7 7 7 . . . . . . 
-. . . . . 7 7 7 7 7 7 . . . . . 
-. . . . . 7 7 7 7 7 7 . . . . . 
-. . . . . 7 f 1 7 7 7 . . . . . 
-. . . . . 7 1 1 7 7 7 . . . . . 
-. . . . . 7 7 7 7 7 7 . . . . . 
-. . . . . . 7 7 7 7 . . . . . . 
-. . . . . . . 7 7 7 7 . d . . . 
-. . . . . . . . . 7 d d d . . . 
-. . . . . . . . 7 d d d 1 . . . 
-. . . . . . . . . d d d 1 . . . 
-. . . . . . . . . . 1 1 1 d . . 
-. . . . . . . . . . . d . . . . 
-`, 50, 0)
-    projectile2.setPosition(0, Math.randomRange(-50, 120))
-}
-let projectile2: Sprite = null
 let Victim: Sprite = null
+let projectile: Sprite = null
 player()
 game.onUpdate(function () {
     projectile()
